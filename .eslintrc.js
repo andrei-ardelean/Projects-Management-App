896a7f7a51b493c3a5f77816361fa0c4c4461360
Razvan Pavel <razvan.e.pavel@gmail.com>
2020-12-16 15:59:01 +0200

    linting standard fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   root: true,
-  extends: [
-    "plugin:vue/vue3-strongly-recommended",
-    "eslint:recommended",
-  ],
+  extends: ["plugin:vue/vue3-strongly-recommended", "eslint:recommended"],
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
 };
