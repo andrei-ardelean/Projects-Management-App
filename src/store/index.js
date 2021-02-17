906400e4b79b2fbccc974cf7a2e53a8cc34a2350
Ardelean Andrei <45100902+Ardealul@users.noreply.github.com>
2021-02-17 15:30:16 +0200

    implement stores for all entities

--- src/store/index.js
+++ src/store/index.js
@@ -8,28 +8,6 @@ import participants from './modules/participants';
 import { createStore } from 'vuex';
 
 export default createStore({
-  // state: {
-  //   projects: []
-  // },
-  // getters: {
-  //   allProjects(state) {
-  //     return state.projects;
-  //   }
-  // },
-  // mutations: {
-  //   setProjects(state, projects) {
-  //     state.projects = projects;
-  //   }
-  // },
-  // actions: {
-  //   setProjects({ commit }){
-  //     axios.get('./projects.json')
-  //       .then(response => {
-  //       commit('setProjects', response.data);
-  //       console.log(response);
-  //     }).catch(response => console.log(response));
-  //   }
-  // },
   modules: {
     projects,
     responsibles,
